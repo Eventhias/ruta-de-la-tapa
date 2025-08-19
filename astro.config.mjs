199fb3e271096e88c8457ca7b359c2297414c99c
JoaquinFG <joaquinferreirag@gmail.com>
2025-08-19 16:51:20 +0100

    img changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://www.eventhias.com/ruta-de-la-tapa-tacoronte",
+  site: "https://www.eventhias.com/ruta-demo-eventhias",
   integrations: [tailwind(), icon()],
 });
